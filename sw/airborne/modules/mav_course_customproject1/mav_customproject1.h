31b96a72e76a02a7538f1784b4e54a4b1aee9e49
rwvos <reinier.vos21@live.com>
2025-03-12 16:41:44 +0100

    custom project 1

---
+++ sw/airborne/modules/mav_course_customproject1/mav_customproject1.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 Matteo Barbera <matteo.barbera97@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+ #ifndef PAPARAZZI_MAV_CUSTOMPROJECT1_H
+ #define PAPARAZZI_MAV_CUSTOMPROJECT1_H
+ 
+ // settings
+extern float oag_color_count_frac;  // obstacle detection threshold as a fraction of total of image
+extern float oag_floor_count_frac;  // floor detection threshold as a fraction of total of image
+extern float oag_max_speed;         // max flight speed [m/s]
+extern float oag_heading_rate;      // heading rate setpoint [rad/s]
+
+ extern void mav_customproject1_init(void);
+ extern void mav_customproject1_periodic(void);
+ 
+ #endif //PAPARAZZI_MAV_CUSTOMPROJECT1_H
+ 
